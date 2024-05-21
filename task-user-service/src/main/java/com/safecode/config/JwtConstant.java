4c708bec039076272ad827d5c6ea0d39d1f298f9
David Trejos <davidtrejoscortes@gmail.com>
2024-05-21 11:51:52 -0500

    Security Configuration with Jason Web Token

---
+++ task-user-service/src/main/java/com/safecode/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.safecode.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY = "fjiowefh85u85u68hwe8hf8re479YY79Y8/)&(85%%/R/%";
+    public static final String JWT_HEADER ="Authorization";
+}
---
+++ task-user-service/src/main/java/com/safecode/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.safecode.repository;
+
+import com.safecode.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    public User findByEmail(String email);
+}
